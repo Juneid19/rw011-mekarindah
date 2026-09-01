03fd80f840979732b0afe8ec4eb822a0e7c48022
Juneid19 <Juneid19@users.noreply.github.com>
2026-09-01 08:52:04 +0700

    Quote runner: 100 quote islami running text di bawah layar + galeri video fix + aduan Telegram

---
+++ public/quote-runner.js
@@ -0,0 +1,134 @@
+(function(){
+var Q=[
+{t:"Sesungguhnya bersama kesulitan ada kemudahan",s:"QS. Al-Insyirah: 6"},
+{t:"Dan janganlah kamu berputus asa dari rahmat Allah. Sesungguhnya tiada berputus asa dari rahmat Allah melainkan orang-orang yang kafir",s:"QS. Yusuf: 87"},
+{t:"Dan Dialah Maha Mengetahui segala sesuatu",s:"QS. Al-Baqarah: 29"},
+{t:"Sesungguhnya Allah tidak akan mengubah keadaan suatu kaum sehingga mereka mengubah keadaan diri mereka sendiri",s:"HR. Ahmad"},
+{t:"Barangsiapa yang menempuh suatu jalan untuk mencari ilmu, maka Allah akan memudahkan baginya jalan menuju surga",s:"HR. Muslim"},
+{t:"Tidak beriman salah seorang di antara kalian hingga ia mencintai saudaranya sebagaimana ia mencintai dirinya sendiri",s:"HR. Bukhari & Muslim"},
+{t:"Perumpamaan orang-orang mukmin dalam hal kasih sayang dan persaudaraan mereka adalah seperti satu tubuh",s:"HR. Muslim"},
+{t:"Janganlah kamu meremehkan kebaikan walau hanya menemu saudaramu dengan wajah yang manis",s:"HR. Muslim"},
+{t:"Orang mukmin yang paling sempurna imannya adalah yang paling baik akhlaknya",s:"HR. Ahmad"},
+{t:"Dunia ini penjara bagi orang mukmin dan surga bagi orang kafir",s:"HR. Muslim"},
+{t:"Jika kamu tidak malu, maka perbuatlah sekehendakmu",s:"HR. Bukhari"},
+{t:"Siapa yang menunjukkan kebaikan, maka ia mendapat pahala seperti orang yang melakukannya",s:"HR. Muslim"},
+{t:"Hendaklah kamu bersikap adil, karena keadilan itu mendekatkan kepada kebaikan",s:"HR. Muslim"},
+{t:"Tidak ada kebaikan pada banyak harta bila tidak dipergunakan dengan benar",s:"Umar bin Khattab"},
+{t:"Hitunglah amal perbuatanmu sebelum dihitung, dan timbanglah amal perbuatanmu sebelum ditimbang",s:"Umar bin Khattab"},
+{t:"Jika kamu malu, maka lakukanlah apa yang benar",s:"Umar bin Khattab"},
+{t:"Kita adalah umat yang diberi kemudahan bukan kesulitan",s:"Umar bin Khattab"},
+{t:"Orang yang paling kuat bukan yang bisa mengalahkan orang lain, tapi yang bisa mengalahkan hawa nafsunya",s:"Umar bin Khattab"},
+{t:"Dunia ini lebih rendah dari bangkai yang disembel di sisi Allah",s:"Ali bin Abi Thalib"},
+{t:"Nilai setiap orang tergantung pada apa yang ia kerjakan dengan baik",s:"Ali bin Abi Thalib"},
+{t:"Jangan jadikan dunia ini sebagai tujuan tertinggimu, karena sesungguhnya kamu melebihkannya dari nilainya",s:"Ali bin Abi Thalib"},
+{t:"Hendaklah kamu menjadi orang yang pandai mencari kebenaran, dan jangan menjadi orang yang pandai mencari keburukan",s:"Ali bin Abi Thalib"},
+{t:"Cukuplah kematian sebagai nasihat bagi kita",s:"Utsman bin Affan"},
+{t:"Seseorang yang beriman itu bukan yang mencela dan mencela, tetapi yang memberi nasihat dan memaafkan",s:"Abu Bakar Ash-Shiddiq"},
+{t:"Kebahagiaan itu ada pada tiga hal: rumah yang layak, istri yang shalihah, dan kendaraan yang nyaman",s:"Abu Bakr Ash-Shiddiq"},
+{t:"Sesungguhnya dalam tubuh ada segumpal daging, jika baik maka baiklah seluruh tubuh, itu adalah hati",s:"HR. Bukhari & Muslim"},
+{t:"Barangsiapa yang beriman kepada Allah dan hari akhir, hendaklah ia berkata yang baik atau diam",s:"HR. Bukhari & Muslim"},
+{t:"Kekuatan itu bukan dari fisik, kekuatan itu dari hati yang ikhlas",s:"Abdullah bin Mas'ud"},
+{t:"Dan berikanlah kabar gembira kepada orang-orang yang sabar, yaitu orang-orang yang ditimpa musibah",s:"QS. Al-Baqarah: 155-156"},
+{t:"Janganlah kamu lemah dan janganlah kamu bersedih hati, sedangkamulah orang-orang yang paling tinggi derajatnya",s:"QS. Ali Imran: 139"},
+{t:"Dan mereka yang berjihad untuk Kami, benar-benar akan Kami tunjukkan jalan-jalan Kami",s:"QS. Al-Ankabut: 69"},
+{t:"Wahai manusia, sesungguhnya Kami menciptakan kamu dari seorang laki-laki dan seorang perempuan",s:"QS. Al-Hujurat: 13"},
+{t:"Sesungguhnya manusia itu diciptakan bersifat gelisah, apabila ia ditimpa keburukan ia berputus asa",s:"QS. Al-Ma'arij: 19"},
+{t:"Padahal mereka tidak disuruh kecuali supaya menyembah Allah dengan mengikhlaskan ketaatan kepada-Nya",s:"QS. Al-Bayyinah: 5"},
+{t:"Wahai manusia, bertakwalah kepada Tuhanmu yang telah menciptakan kamu dari satu jiwa",s:"QS. An-Nisa: 1"},
+{t:"Hai anak Adam, pakailah pakaianmu yang bagus pada setiap kali memasuki masjid",s:"QS. Al-A'raf: 31"},
+{t:"Wahai orang-orang yang beriman, peliharalah dirimu dan keluargamu dari api neraka",s:"QS. At-Tahrim: 6"},
+{t:"Sesungguhnya orang-orang yang beriman itu bersaudara, karena itu damaikanlah antara dua saudaramu",s:"QS. Al-Hujurat: 10"},
+{t:"Barangsiapa yang mengerjakan kebaikan, maka untuk dirinya sendiri. Dan barangsiapa yang mengerjakan kejahatan, maka untuk dirinya sendiri",s:"QS. Fussilat: 46"},
+{t:"Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menjadikan untukmu pasangan hidup dari jenismu sendiri",s:"QS. Ar-Rum: 21"},
+{t:"Katakanlah: Sesungguhnya aku ini manusia biasa seperti kamu, diwahyukan kepadaku bahwa Tuhanmu adalah Tuhan Yang Maha Esa",s:"QS. Al-Kahfi: 110"},
+{t:"Allah tidak membebani seseorang melainkan sesuai dengan kesanggupannya",s:"QS. Al-Baqarah: 286"},
+{t:"Maka sesungguhnya bersama kesukaran itu ada kemudahan. Sesungguhnya bersama kesukaran ada kemudahan",s:"QS. Asy-Syarh: 5-6"},
+{t:"Dan apa saja nikmat yang kamu peroleh adalah dari sisi Allah",s:"QS. An-Nahl: 53"},
+{t:"Sesungguhnya Allah beserta orang-orang yang sabar",s:"QS. Al-Baqarah: 153"},
+{t:"Dan jadikanlah sabar dan shalat sebagai penolongmu",s:"QS. Al-Baqarah: 45"},
+{t:"Dan sungguh akan Kami berikan cobaan kepadamu, dengan sedikit ketakutan, lapar, kekurangan harta, jiwa, dan buah-buahan",s:"QS. Al-Baqarah: 155"},
+{t:"Tidaklah seorang mukmin menanamkan tanaman, lalu ia dimakan manusia, burung, atau binatang, kecuali ia mendapatkan sedekah",s:"HR. Bukhari & Muslim"},
+{t:"Tujuh orang yang akan dinaungi Allah dalam naungan-Nya pada hari yang tidak ada naungan selain naungan-Nya",s:"HR. Bukhari & Muslim"},
+{t:"Dua rahmat yang tidak bisa didapatkan seorang hamba setelah rahmat dua orang tuanya",s:"HR. Tirmidzi"},
+{t:"Siapa yang membangun masjid karena Allah, maka Allah membangun untuknya serupa rumah di surga",s:"HR. Bukhari & Muslim"},
+{t:"Sesungguhnya setiap amal tergantung pada niat. Sesungguhnya setiap orang mendapat apa yang ia niatkan",s:"HR. Bukhari & Muslim"},
+{t:"Orang mukmin itu mencintai tiga hal: menempelkan hatinya kepada Tuhannya, memperbaiki makanannya, dan mencintai saudaranya",s:"HR. Thabrani"},
+{t:"Sesungguhnya Allah itu Maha Pemurah, menyukai kebaikan, menyukai orang yang memuliakan tetangga",s:"HR. Ahmad"},
+{t:"Jauhilah yang meragukan, karena sesungguhnya yang paling jujur pun bisa terkena keraguan",s:"HR. Tirmidzi & Nasa'i"},
+{t:"Barangsiapa yang menghendaki kebaikan, diberi ketabahan. Dan barangsiapa yang menghendaki kesenangan, diberi kefakiran",s:"HR. Bukhari & Muslim"},
+{t:"Diantara tanda kebaikan seseorang adalah ia meninggalkan apa yang tidak menyangkutnya",s:"HR. Tirmidzi"},
+{t:"Sebaik-baik manusia adalah yang paling bermanfaat bagi manusia lainnya",s:"HR. Ahmad"},
+{t:"Shalat lima waktu dan Jumat ke Jumat berikutnya adalah penghapus dosa di antaranya",s:"HR. Muslim"},
+{t:"Barangsiapa yang mengerjakan shalat Subuh maka ia berada dalam perlindungan Allah",s:"HR. Muslim"},
+{t:"Sesungguhnya Allah itu Maha Indah, menyukai keindahan",s:"HR. Muslim"},
+{t:"Hendaklah kamu membersihkan hatimu, karena sesungguhnya hati itulah yang paling berat di timbangan",s:"HR. Al-Baihaqi"},
+{t:"Barangsiapa yang memudahkan orang yang kesulitan, nanti Allah memudahkan urusannya di dunia dan akhirat",s:"HR. Muslim"},
+{t:"Tidak ada kemuliaan bagi orang yang tidak mempunyai ilmu",s:"Umar bin Khattab"},
+{t:"Belajarlah sebelum kamu menjadi pemimpin, agar kamu tidak terhina di bawah langit ini",s:"Umar bin Khattab"},
+{t:"Duduklah di tempat yang rendah agar kamu naik ke tempat yang tinggi",s:"Umar bin Khattab"},
+{t:"Orang bodoh itu tidak dianggap dalam musyawarat",s:"Umar bin Khattab"},
+{t:"Kamu tidak akan sampai pada kemuliaan kecuali dengan menempuh jalan kesulitan",s:"Umar bin Khattab"},
+{t:"Ujian itu memberi kesempatan bagi orang yang diuji untuk menunjukkan kebenaran",s:"Umar bin Khattab"},
+{t:"Janganlah kamu menjadi pemimpin jika kamu tidak sanggup menanggung beban",s:"Umar bin Khattab"},
+{t:"Kebodohan itu adalah musuh terbesar bagi manusia, kecuali bagi yang dijaga oleh Allah",s:"Ali bin Abi Thalib"},
+{t:"Waktu itu seperti pedang, jika kamu tidak memotongnya, ia akan memotongmu",s:"Ali bin Abi Thalib"},
+{t:"Orang yang paling celaka adalah orang yang menjual akhiratnya dengan dunia orang lain",s:"Ali bin Abi Thalib"},
+{t:"Tidak ada kekayaan yang lebih besar dari akal",s:"Ali bin Abi Thalib"},
+{t:"Sabar itu ada dua macam: sabar atas apa yang kamu benci dan sabar menahan diri dari apa yang kamu cintai",s:"Ali bin Abi Thalib"},
+{t:"Hati itu seperti tanah yang subur, tumbuh di atasnya apa yang ditanamkan",s:"Ali bin Abi Thalib"},
+{t:"Janganlah kamu menyesali atas apa yang telah berlalu, kecuali tiga hal: kebaikan yang terlewat, kejelekan yang terlakukan, dan waktu yang terbuang",s:"Ali bin Abi Thalib"},
+{t:"Barangsiapa yang merendahkan dirinya karena Allah, niscaya Allah akan mengangkat derajatnya",s:"Ali bin Abi Thalib"},
+{t:"Saya tidak menghalalkan kebatilan untuk menolong kebenaran",s:"Abu Bakar Ash-Shiddiq"},
+{t:"Ketahuilah bahwa apa yang telah terlewat tidak akan kembali, dan apa yang akan datang tidak kamu ketahui",s:"Abu Bakar Ash-Shiddiq"},
+{t:"Janganlah kalian memandang rendah kepada orang kaya karena hartanya, karena hartanya bukan dari akalnya",s:"Hasan Al-Basri"},
+{t:"Cukup bagimu dunia sebagai bekal akhirat, dan cukup bagimu akhirat sebagai bekal dunia",s:"Hasan Al-Basri"},
+{t:"Barangsiapa yang mengenal Allah, hatinya menjadi tenang",s:"Hasan Al-Basri"},
+{t:"Seorang mukmin di dunia ini seperti orang asing, ia tidak merasa sedih karena dihinakan dan tidak gembira karena dihormati",s:"Hasan Al-Basri"},
+{t:"Ilmu tanpa amal seperti pohon tanpa buah",s:"Ibnu Katsir"},
+{t:"Barangsiapa yang meninggalkan sesuatu karena Allah, niscaya Allah menggantikan dengan yang lebih baik",s:"Ibnu Katsir"},
+{t:"Janganlah kamu marah ketika kamu tidak mampu mengubah sesuatu, karena marah tidak berguna",s:"Imam Syafi'i"},
+{t:"Ilmu adalah sahabat yang tidak mempunyai kepentingan dan tidak akan meninggalkanmu dalam kesepian",s:"Imam Syafi'i"},
+{t:"Jika kamu tidak mendapat kebaikan selain Allah, maka cukuplah Allah itu sebagai kebaikan",s:"Ibnu Taimiyyah"},
+{t:"Orang yang paling bahagia adalah yang paling banyak mengingat kematian",s:"Ibnu Taimiyyah"},
+{t:"Kebahagiaan sejati adalah ketenangan hati dan kelapangan dada",s:"Ibnu Taimiyyah"},
+{t:"Dunia itu ibarat mayat, orang yang mencintainya seperti orang yang mencintai mayat",s:"Imam Al-Ghazali"},
+{t:"Keikhlasan itu membebaskan dari segala belenggu",s:"Imam Al-Ghazali"},
+{t:"Hati itu seperti kaca, kalau kotor maka bayangannya pun kotor, kalau bersih maka bayangannya pun bersih",s:"Imam Al-Ghazali"},
+{t:"Aku tidak akan memenangkan pertempuran dengan jumlah pasukan, tapi dengan keyakinan di hati",s:"Shalahuddin Al-Ayyubi"},
+{t:"Kemenangan sejati adalah mengalahkan hawa nafsu sendiri, bukan mengalahkan musuh di medan perang",s:"Shalahuddin Al-Ayyubi"},
+{t:"Akhlak yang baik adalah sebaik-baiknya ilmu dan kebodohan adalah seburuk-buruknya kebodohan",s:"HR. Ad-Dailami"},
+{t:"Sampaikan dariku walau satu ayat",s:"HR. Bukhari"},
+{t:"Dunia ini kurang dari nilai seekor nyamuk di sisi Allah",s:"HR. Tirmidzi"},
+{t:"Hendaklah kamu berbuat baik kepada sesama manusia, niscaya Allah berbuat baik kepadamu",s:"HR. Thabrani"},
+{t:"Shalat itu tiang agama, siapa yang mendirikannya maka ia mendirikan agama, dan siapa yang merobohkannya maka ia merobohkan agama",s:"HR. Baihaqi"},
+{t:"Sesungguhnya Allah itu Maha Pengampun, menyukai orang yang memaafkan",s:"HR. Ahmad"}
+];
+var st=document.createElement("style");
+st.textContent="@keyframes qrscroll{from{transform:translateX(100vw)}to{transform:translateX(-100%)}}";
+document.head.appendChild(st);
+var d=document.createElement("div");
+d.id="qbar";
+d.style.cssText="position:fixed;bottom:0;left:0;right:0;height:42px;background:linear-gradient(90deg,rgba(13,122,58,0.97),rgba(8,90,40,0.97));color:#fff;z-index:99999;font-family:system-ui,sans-serif;box-shadow:0 -2px 12px rgba(0,0,0,0.25);overflow:hidden;display:flex;align-items:center;";
+var wr=document.createElement("div");
+wr.style.cssText="display:inline-block;white-space:nowrap;animation:qrscroll 13s linear;padding:0 20px;";
+d.appendChild(wr);
+var cl=document.createElement("div");
+cl.innerHTML="&#10005;";
+cl.style.cssText="position:absolute;right:8px;top:50%;transform:translateY(-50%);cursor:pointer;opacity:0.5;font-size:13px;width:26px;height:26px;display:flex;align-items:center;justify-content:center;border-radius:50%;background:rgba(255,255,255,0.15);flex-shrink:0;";
+cl.onmouseover=function(){this.style.opacity="1"};
+cl.onmouseout=function(){this.style.opacity="0.5"};
+cl.onclick=function(){d.style.display="none";sessionStorage.setItem("qbar","off")};
+d.appendChild(cl);
+if(sessionStorage.getItem("qbar")==="off")d.style.display="none";
+document.body.appendChild(d);
+var i=Math.floor(Math.random()*Q.length);
+function show(){
+wr.style.animation="none";
+wr.offsetHeight;
+wr.innerHTML='\u201C '+Q[i].t+' \u201D <b style="opacity:0.7;margin-left:8px;font-size:11px">\u2014 '+Q[i].s+"</b>";
+wr.style.animation="qrscroll 13s linear";
+i=(i+1)%Q.length;
+}
+show();
+setInterval(show,13000);
+})();
